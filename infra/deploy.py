aa04812e5f6e2e013fe1faf02329c4bf1b8c2419
Ralf Sieger <sieger@fzi.de>
2025-10-21 15:31:14 +0200

    added basic measurement tools

--- infra/deploy.py
+++ infra/deploy.py
@@ -4,10 +4,21 @@ import io
 from pyinfra.operations import apt, files, server
 from pyinfra import host
 
+apt.update(
+        name="Update apt repositories",
+        _sudo=True,
+        )
+
 apt.packages(
     name="Install base packages",
-    packages=["fish", "vim", "less", "tmux", "iputils-ping", "iptables", "wget", "git", "cpufrequtils", "lm-sensors"],
+    packages=["fish", "vim", "less", "tmux", "iputils-ping", "iptables", "wget", "git", "lm-sensors"],
     no_recommends=True,
     _sudo=True,
 )
 
+apt.packages(
+    name="Install measurement packages",
+    packages=["linux-cpupower"],
+    no_recommends=True,
+    _sudo=True,
+)
