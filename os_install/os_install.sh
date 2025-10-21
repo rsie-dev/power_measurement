19cc0ae2cd2604f8afce019846588663007df6dc
Ralf Sieger <sieger@fzi.de>
2025-10-21 14:45:42 +0200

    removed superfluous python files

--- os_install/os_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-script_dir=`dirname $(readlink -f "$0")`
-#echo script dir: $script_dir
-activate=$script_dir/venv/bin/activate
-. $activate
-py=$script_dir/os_install.py
-python -O $py "$@"
