62122b22b653eb8f434b163f12b29c748c545ed6
Ralf Sieger <sieger@fzi.de>
2025-10-27 16:01:06 +0100

    renamed to better match purpose

--- collector/collector.py
+++ collector/collector.py
@@ -139,13 +139,13 @@ class Collector:
         id_group.add_argument('--id', help="Device vendorid:productid")
         id_group.add_argument('--serial-number', type=lambda x: int(x, 16), help="Device serial number")
 
-        parser_srv = subparsers.add_parser('collector', parents=[id_parser], help="starts data collection")
-        parser_srv.add_argument("--host", default=self._get_default_host(), help="Server listening host" + default)
-        parser_srv.add_argument("-p", "--port", type=int, default=10000, help="Server listening port" + default)
-        parser_srv.add_argument('--system', default="system.csv", help="System data file name" + default)
-        parser_srv.add_argument('--electrical', default="electrical.csv", help="Electrical data file name" + default)
-        parser_srv.add_argument("--latest-only", action="store_true", help="Only log the latest electrical measurement per batch")
-        parser_srv.set_defaults(func=self._collector)
+        parser_collect = subparsers.add_parser('collector', parents=[id_parser], help="starts data collection")
+        parser_collect.add_argument("--host", default=self._get_default_host(), help="Server listening host" + default)
+        parser_collect.add_argument("-p", "--port", type=int, default=10000, help="Server listening port" + default)
+        parser_collect.add_argument('--system', default="system.csv", help="System data file name" + default)
+        parser_collect.add_argument('--electrical', default="electrical.csv", help="Electrical data file name" + default)
+        parser_collect.add_argument("--latest-only", action="store_true", help="Only log the latest electrical measurement per batch")
+        parser_collect.set_defaults(func=self._collector)
 
         args = parser.parse_args()
 
