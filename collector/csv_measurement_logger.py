caadf4399878994c13fcd9f52c31de110ed2a985
Ralf Sieger <sieger@fzi.de>
2025-10-27 09:39:08 +0100

    added host and metric name

--- collector/csv_measurement_logger.py
+++ collector/csv_measurement_logger.py
@@ -6,7 +6,7 @@ from server.metrics import Measurement
 
 
 class CSVDataLogger(MeasurementLogger):
-    FIELD_NAMES = ["timestamp", "rel time"]
+    FIELD_NAMES = ["timestamp", "rel time", "host", "name"]
 
     def __init__(self, path: Path):
         self._stream = path.open(mode="w", encoding="utf-8")
@@ -31,12 +31,7 @@ class CSVDataLogger(MeasurementLogger):
         entry = {
             "timestamp": f"{measurement.timestamp.isoformat(timespec="milliseconds")}",
             "rel time": f"{rel_time.total_seconds():7.2f}",
-        #     "voltage_V": f"{data.voltage:7.5f}",
-        #     "current_A": f"{data.current:7.5f}",
-        #     "dp_V": f"{data.dp:5.3f}",
-        #     "dn_V": f"{data.dn:5.3f}",
-        #     "temp_C_ema": f"{data.temperature:6.3f}",
-        #     "energy_Ws": f"{data.energy:.6f}",
-        #     "capacity_As": f"{data.capacity:.6f}",
+            "host": f"{measurement.tags["host"]}",
+            "name": f"{measurement.name}",
         }
         self._writer.writerow(entry)
