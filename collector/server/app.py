22e020540939d548ffd9e0c22ec74ea10562e443
Ralf Sieger <sieger@fzi.de>
2025-10-28 14:54:18 +0100

    removed logging calls

--- collector/server/app.py
+++ collector/server/app.py
@@ -11,23 +11,13 @@ logger = logging.getLogger('server.main')
 def create_app(measurement_logger: MeasurementLogger):
     app = FastAPI()
 
-    counter = 0
-
     @app.post("/measurement/single/")
     def add_line(measurement: SystemMeasurement) -> SystemMeasurement:
-        nonlocal counter
-        counter = counter + 1
-        logger.info("received measurement %d\n%s" % (counter, measurement))
         measurement_logger.log(measurement)
         return measurement
 
-    batch_counter = 0
-
     @app.post("/measurement/batch/")
     def add_batch(metrics: Metrics) -> Metrics:
-        nonlocal batch_counter
-        batch_counter = batch_counter + 1
-        logger.info("received batch %d" % (batch_counter, ))
         for measurement in metrics.metrics:
             measurement_logger.log(measurement)
         return metrics
