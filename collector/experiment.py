897486f08367c7310444a7e0ab8c7b6800e088b9
Ralf Sieger <sieger@fzi.de>
2025-10-28 15:21:20 +0100

    added device subcommands

--- collector/experiment.py
+++ collector/experiment.py
@@ -12,7 +12,7 @@ import ifaddr
 from signal_handler import SignalHandler
 from system_meter import ShutdownHandler
 from csv_system_logger import CSVSystemLogger
-from usb_meter import devices_by_vid_pid, devices_by_serial_number
+from usb_meter import all_devices, devices_by_vid_pid, devices_by_serial_number
 from usb_meter import USBMeter, StopProvider
 from csv_electrical_logger import CSVElectricLogger
 
@@ -53,14 +53,6 @@ class Collector:
                 del yaml_config['handlers']['file']
             logging.config.dictConfig(yaml_config)
 
-    def _system_collector(self, metrics_server, args):
-        self._logger.debug("REST system_meter start")
-        try:
-            with CSVSystemLogger(Path(args.system)) as dl:
-                metrics_server.run(args, dl)
-        finally:
-            self._logger.debug("REST system_meter shut down")
-
     def _split_id(self, id):
         tokens = id.split(":")
         return int(tokens[0], 16), int(tokens[1], 16)
@@ -88,6 +80,30 @@ class Collector:
         if next(devices, None):
             raise RuntimeError("Too many devices found with: %s" % self._get_id_description(args))
         return device
+
+    def _device_list(self, args):
+        devices = all_devices()
+        self._logger.info("Available devices:")
+        for device in devices:
+            sn = device.serial_number
+            product = device.product_name
+            manufacturer = device.manufacturer_name
+            self._logger.info(f"- {device.device_info.vid:x}:{device.device_info.pid:x} {manufacturer} {product} (type: {device.device_info.model.name} SN: {sn})")
+
+    def _device_show(self, args):
+        device = self._find_device(args)
+        self._logger.info(f"Vendor ID:     {device.device_info.vid:x}")
+        self._logger.info(f"Product ID:    {device.device_info.pid:x}")
+        self._logger.info(f"Type:          {device.device_info.model.name}")
+        self._logger.info(f"Serial number: {device.serial_number}")
+
+    def _system_collector(self, metrics_server, args):
+        self._logger.debug("REST system_meter start")
+        try:
+            with CSVSystemLogger(Path(args.system)) as dl:
+                metrics_server.run(args, dl)
+        finally:
+            self._logger.debug("REST system_meter shut down")
 
     def _electric_collector(self, usb_meter, args):
         self._logger.info("USB meter start")
@@ -138,6 +154,14 @@ class Collector:
         id_group = id_parser.add_mutually_exclusive_group(required=True)
         id_group.add_argument('--id', help="Device vendorid:productid")
         id_group.add_argument('--serial-number', type=lambda x: int(x, 16), help="Device serial number")
+
+        parser_device = subparsers.add_parser('device', help="device commands")
+        device_subparsers = parser_device.add_subparsers(required=True, dest="subcommand", title='subcommands',
+                                                         description='valid subcommands', help='sub-command help')
+        parser_device_list = device_subparsers.add_parser('list', help="List devices")
+        parser_device_list.set_defaults(func=self._device_list)
+        parser_device_show = device_subparsers.add_parser('show', parents=[id_parser], help="Show device details")
+        parser_device_show.set_defaults(func=self._device_show)
 
         parser_run = subparsers.add_parser('run', parents=[id_parser], help="runs an experiment")
         parser_run.add_argument("--host", default=self._get_default_host(), help="Server listening host" + default)
