70d94df4fce2234db0d2a594a41178a7e8f80b45
Ralf Sieger <sieger@fzi.de>
2025-11-01 13:49:43 +0100

    created interface

--- experiment/app/experiment/steps/experiment_environment.py
+++ experiment/app/experiment/steps/experiment_environment.py
@@ -1,14 +1,18 @@
+from abc import ABC, abstractmethod
 from pathlib import Path
 
 from app.common import ShutdownHandler
 
 
-class ExperimentEnvironment:
+class ExperimentEnvironment(ABC):
+    @abstractmethod
     def get_resources_path(self) -> Path:
-        return None
-
-    def add_shutdown_handler(self, handler: ShutdownHandler):
         pass
 
+    @abstractmethod
+    def add_shutdown_handler(self, handler: ShutdownHandler) -> None:
+        pass
+
+    @abstractmethod
     def register_for_system_meter(self, host: str) -> None:
         pass
