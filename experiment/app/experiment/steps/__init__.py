8fc959809d58b4725588c50f722613356f689e13
Ralf Sieger <sieger@fzi.de>
2025-11-01 13:41:09 +0100

    extracted USB meter step into separate module

--- experiment/app/experiment/steps/__init__.py
+++ experiment/app/experiment/steps/__init__.py
@@ -1,5 +1,6 @@
 from .steps import Step
-from .steps import USBMeterStep, HostCommandStep
+from .steps import HostCommandStep
+from .usb_meter_step import USBMeterStep
 from .system_metrics_step import RegisterForSystemMetricsStep, SystemMetricsStep
 
 from .experiment_environment import ExperimentEnvironment
---
+++ experiment/app/experiment/steps/usb_meter_step.py
@@ -0,0 +1,55 @@
+import logging
+import threading
+from concurrent.futures import Executor
+
+from app.usb_meter import devices_by_serial_number, USBMeter
+from app.usb_meter.device import Device
+from .steps import Step, ExperimentEnvironment
+from .csv_electrical_logger import CSVElectricLogger
+from .signal_stop_provider import SignalStopProvider
+
+
+class USBMeterStep(Step):
+    def __init__(self, host: str, serial_number: str):
+        super().__init__("USB meter")
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._host = host
+        self._serial_number = serial_number
+        self._usb_meter = None
+        self._electrical_log = None
+        self._stop_provider = SignalStopProvider()
+        self._start_timeout = 3
+
+    def _find_device(self) -> Device:
+        devices = devices_by_serial_number(self._serial_number)
+        device = next(devices, None)
+        if not device:
+            raise RuntimeError("No devices found with: %s" % self._serial_number)
+        if next(devices, None):
+            raise RuntimeError("Too many devices found with: %s" % self._serial_number)
+        return device
+
+    def _electric_collector(self, usb_meter, electrical_log, event):
+        self._logger.info("USB meter start")
+        event.set()
+        try:
+            with CSVElectricLogger(electrical_log, latest_only=True) as data_logger:
+                usb_meter.run(data_logger)
+        finally:
+            self._logger.info("USB meter shut down")
+
+    def init(self, environment: ExperimentEnvironment):
+        device = self._find_device()
+        environment.add_shutdown_handler(self._stop_provider)
+        self._usb_meter = USBMeter(device=device, stop_provider=self._stop_provider, use_crc=True)
+        self._usb_meter.setup_device()
+        self._electrical_log = environment.get_resources_path() / "electrical.csv"
+
+    def start(self, executor: Executor):
+        event = threading.Event()
+        future = executor.submit(self._electric_collector, self._usb_meter, self._electrical_log, event)
+        event.wait(self._start_timeout)
+        return future
+
+    def stop(self):
+        self._stop_provider.shut_down(False)
