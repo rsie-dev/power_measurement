fd89e0ac2485f385455ec307bebea367323a83ad
Ralf Sieger <sieger@fzi.de>
2025-11-01 14:54:23 +0100

    added separate experiment logfile

--- experiment/app/run/runner.py
+++ experiment/app/run/runner.py
@@ -1,6 +1,8 @@
 import logging
 from pathlib import Path
 
+from ruamel.yaml import YAML
+
 from .experiment_loader import ExperimentLoader
 
 
@@ -9,6 +11,30 @@ class Runner:
         self._logger = logging.getLogger(self.__class__.__name__)
         self._resources = resources
 
+    def _get_app_folder(self):
+        script = Path(__file__).resolve()
+        folder = script.parent.parent
+        return folder
+
+    def _get_logging_config(self):
+        folder = self._get_app_folder()
+        config = folder / 'logging.yaml'
+
+        with open(config, "rt", encoding="UTF_8") as f:
+            yaml = YAML(typ="safe")
+            yaml_config = yaml.load(f)
+            return yaml_config
+
+    def _add_logfile(self, logfile: Path):
+        config = self._get_logging_config()
+        handler = logging.FileHandler(logfile, mode="w")
+        handler.setLevel(logging.DEBUG)
+        config_file_formatter = config["formatters"]["file"]
+        config_file_formatter["fmt"] = config_file_formatter.pop("format")
+        formatter = logging.Formatter(**(dict(config_file_formatter)))
+        handler.setFormatter(formatter)
+        logging.getLogger().addHandler(handler)
+
     def run_experiment(self, args):
         experiment_loader = ExperimentLoader()
         experiment_module = Path(args.experiment[0])
@@ -16,6 +42,7 @@ class Runner:
         resources = self._resources / experiment_module.stem
         self._logger.info("experiment resources: %s", resources.relative_to(Path.cwd()))
         resources.mkdir(parents=True, exist_ok=True)
+        self._add_logfile(resources / "experiment.log")
         self._logger.info("Experiment start: %s", experiment_module.stem)
         try:
             experiment.run(resources)
