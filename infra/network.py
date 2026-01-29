a9bff505334081ffa9307d4df757b0d01b78b5d9
Ralf Sieger <sieger@fzi.de>
2026-01-29 13:43:50 +0100

    added basic dhcp support

--- infra/network.py
+++ infra/network.py
@@ -6,14 +6,56 @@ from pyinfra.api import deploy
 def network_base():
     apt.packages(
         name="Install core network tools",
-        packages = ["iputils-ping", "iptables", "netcat-openbsd"],
-        no_recommends = True,
-        _sudo = True,
+        packages=["iputils-ping", "iptables", "netcat-openbsd"],
+        no_recommends=True,
+        _sudo=True,
     )
 
     apt.packages(
         name="Install wifi tools",
-        packages = ["wireless-tools", "wavemon"],
-        no_recommends = True,
-        _sudo = True,
+        packages=["wireless-tools", "wavemon"],
+        no_recommends=True,
+        _sudo=True,
     )
+
+
+@deploy("DhcpServer")
+def dhcp_server():
+    apt.packages(
+        name="Install DHCP server",
+        packages=["isc-dhcp-server"],
+        no_recommends=True,
+        _sudo=True,
+    )
+
+    # ToDo configure
+    # option domain-name "swamp.de";
+    # subnet 192.168.3.0 netmask 255.255.255.0 {
+    #   range 192.168.3.200 192.168.3.220;
+    #   option broadcast-address 192.168.3.255;
+    #   option routers 192.168.3.1;
+    #   option domain-name-servers 192.168.3.1;
+    #   option ntp-servers    192.168.3.1;
+    # }
+
+    # ToDo take IP addresses from inventory
+    # host raspi5_4 {
+    #   #hardware ethernet 60:7d:09:01:f7:40;
+    #   hardware ethernet 2c:cf:67:42:55:be;
+    #   fixed-address 192.168.3.202;
+    # }
+    #
+    # host radxax4 {
+    #   hardware ethernet 10:02:b5:86:04:b7;
+    #   fixed-address 192.168.3.203;
+    # }
+    #
+    # host visionfive2 {
+    #   hardware ethernet 6c:cf:39:00:85:3e;
+    #   fixed-address 192.168.3.204;
+    # }
+    #
+    # host visionfive2_lite {
+    #   hardware ethernet 6c:cf:39:00:88:a6;
+    #   fixed-address 192.168.3.205;
+    # }
