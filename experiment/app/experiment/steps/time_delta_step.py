99edbbb44beb20dfcf494993d0e4f00304a2531b
Ralf Sieger <sieger@fzi.de>
2026-02-18 14:43:52 +0100

    store the DUT time delta as ISO 8601 duration per run

--- experiment/app/experiment/steps/time_delta_step.py
+++ experiment/app/experiment/steps/time_delta_step.py
@@ -3,7 +3,11 @@ import datetime
 
 import ntplib
 import humanize
+import isodate
 
+from .experiment_environment import ExperimentEnvironment
+from .experiment_measurement import ExperimentMeasurement
+from .experiment_resources import ExperimentResources
 from .experiment_runtime import ExperimentRuntime
 from .step import Step
 
@@ -14,16 +18,27 @@ class TimeDeltaStep(Step):
         self._logger = logging.getLogger(self.__class__.__name__)
         self._host_name = host_name
         self._host = host
+        self._time_delta_path = None
+
+    def prepare(self, environment: ExperimentEnvironment, measurement: ExperimentMeasurement,
+                resources: ExperimentResources):
+        self._time_delta_path = resources.metrics_resources_path() / "dut_time_delta.txt"
 
     def execute(self, runtime: ExperimentRuntime):
-        self._get_ntp_delta()
+        offset_delta = self._get_ntp_delta()
+
+        microseconds = offset_delta / datetime.timedelta(microseconds=1)
+        status = "behind" if microseconds < 0 else "ahead"
+        delta_str = humanize.precisedelta(offset_delta, minimum_unit="microseconds")
+        self._logger.info("Host %s is %s %s", self._host, status, delta_str)
+
+        with self._time_delta_path.open(mode="w", encoding="utf-8") as f:
+            iso_str = isodate.duration_isoformat(offset_delta)
+            f.write(f'delta = "{iso_str}"')
 
     def _get_ntp_delta(self):
         ntp_client = ntplib.NTPClient()
         self._logger.debug("get time diff to: %s", self._host)
         response = ntp_client.request(self._host, version=4)  # v4 is common
-        offset_delta = datetime.timedelta(seconds = response.offset)
-        status = "behind" if response.offset < 0 else "ahead"
-        delta_str = humanize.precisedelta(offset_delta, minimum_unit="microseconds")
-        self._logger.info("Host %s is %s %s", self._host, status, delta_str)
+        offset_delta = datetime.timedelta(seconds=response.offset)
         return offset_delta
