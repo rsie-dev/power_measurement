a76534ed5942390d309b3f6c4e91f93d7ad0b484
Ralf Sieger <sieger@fzi.de>
2026-03-06 08:18:20 +0100

    added unit tests

--- experiment/app/run/commands/timed_command_test.py
+++ experiment/app/run/commands/timed_command_test.py
@@ -1,7 +1,13 @@
-
 from .timed_command import _parse_time_line
 
-def test_parse_time_line_short():
-    actual_entry = _parse_time_line("real 0.00")
 
-    assert actual_entry == ("real", 0.0)
+def test_parse_time_line_short():
+    actual_entry = _parse_time_line("real 0.10")
+
+    assert actual_entry == ("real", 0.1)
+
+
+def test_parse_time_line_long():
+    actual_entry = _parse_time_line("real 651.00")
+
+    assert actual_entry == ("real", 651.00)
