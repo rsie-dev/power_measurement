493f02680f736e01644247fde1e3655f7674a677
Ralf Sieger <sieger@fzi.de>
2026-03-10 10:26:12 +0100

    fixed missing whitespace

--- experiment/app/run/commands/__init__.py
+++ experiment/app/run/commands/__init__.py
@@ -2,7 +2,7 @@ from .executor_command import ExecutorCommand
 from .delay_command import DelayCommand
 from .timed_command import TimedCommand
 from .composite_command import CompositeCommand
-from.file_stat_command import FileStatCommand
+from .file_stat_command import FileStatCommand
 
 __all__ = [
     "ExecutorCommand",
