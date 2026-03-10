4e990c0b8607b3a6ea47d7d77b031578fc262c80
Ralf Sieger <sieger@fzi.de>
2026-03-10 08:52:37 +0100

    added composite command

--- experiment/app/run/commands/__init__.py
+++ experiment/app/run/commands/__init__.py
@@ -1,9 +1,11 @@
 from .executor_command import ExecutorCommand
 from .delay_command import DelayCommand
 from .timed_command import TimedCommand
+from .composite_command import CompositeCommand
 
 __all__ = [
     "ExecutorCommand",
     "DelayCommand",
     "TimedCommand",
+    "CompositeCommand",
 ]
