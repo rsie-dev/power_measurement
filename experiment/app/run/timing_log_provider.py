190b7981644840f27633eef24d762047fe1c7153
Ralf Sieger <sieger@fzi.de>
2026-03-11 08:56:55 +0100

    moved to a more generic LogProvider

--- experiment/app/run/timing_log_provider.py
+++ experiment/app/run/timing_log_provider.py
@@ -1,24 +1,28 @@
+import logging
 from pathlib import Path
 from typing import ContextManager
 from contextlib import contextmanager
 
 from app.experiment.steps import LogProvider
-from app.experiment.log import logger, LogDispatcher
+from app.experiment.log import logger, BaseLogger, LogDispatcher
 from app.experiment.log import TimingEntry, CSVTimingLogger
 
 
 class TimingLogProvider(LogProvider):
-    def __init__(self, timing_dispatcher: LogDispatcher[TimingEntry], formatter):
-        self._timing_dispatcher = timing_dispatcher
+    def __init__(self, log_dispatcher: LogDispatcher[TimingEntry], formatter: logging.Formatter):
+        self._log_dispatcher = log_dispatcher
         self._formatter = formatter
 
     @contextmanager
     def start_log(self, resource_path: Path) -> ContextManager:
-        timings_log = resource_path / "timings.csv"
-
-        with logger(CSVTimingLogger(timings_log, self._formatter)) as data_logger:
-            self._timing_dispatcher.register_logger(data_logger)
+        base_logger = self._create_logger(resource_path, self._formatter)
+        with logger(base_logger) as data_logger:
+            self._log_dispatcher.register_logger(data_logger)
             try:
                 yield data_logger
             finally:
-                self._timing_dispatcher.unregister_logger(data_logger)
+                self._log_dispatcher.unregister_logger(data_logger)
+
+    def _create_logger(self, resource_path: Path, formatter: logging.Formatter, context: dict = None) -> BaseLogger:
+        timings_log = resource_path / "timings.csv"
+        return CSVTimingLogger(timings_log, formatter)
