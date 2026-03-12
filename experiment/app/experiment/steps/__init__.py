2e655951d5b7e7c8afa4191b20f49a24ba86255a
Ralf Sieger <sieger@fzi.de>
2026-03-12 10:03:18 +0100

    moved HostCommandStep into separate module

--- experiment/app/experiment/steps/__init__.py
+++ experiment/app/experiment/steps/__init__.py
@@ -1,5 +1,6 @@
 from .step import Step, InitStep
-from .host_command_step import WarmupCommandStep, HostCommandStep
+from .host_command_step import WarmupCommandStep
+from .measurement_step import HostCommandStep
 from .system_metrics_step import SystemMetricsClientStep
 from .time_delta_step import TimeDeltaStep
 from .hostname_validation_step import HostnameValidationStep
@@ -8,7 +9,8 @@ from .upload_step import UploadStep
 from .delete_step import DeleteStep
 
 __all__ = ["Step", "InitStep",
-           "WarmupCommandStep", "HostCommandStep",
+           "WarmupCommandStep",
+           "HostCommandStep",
            "SystemMetricsClientStep",
            "TimeDeltaStep",
            "HostnameValidationStep", "HostnameInfoStep",
---
+++ experiment/app/experiment/steps/measurement_step.py
@@ -0,0 +1,78 @@
+import logging
+from contextlib import ExitStack
+from pathlib import Path
+from concurrent.futures import Executor
+from dataclasses import dataclass
+
+from fabric import Connection
+
+from app.api import Command
+from app.common import SSHHost
+from app.experiment.base import ExperimentEnvironment
+from app.experiment.base import ExperimentResources
+from app.experiment.steps.measurement import measure, Measurement
+from app.experiment.log import LogProvider
+from .host_command_step import BaseHostCommandStep
+
+
+class HostCommandStep(BaseHostCommandStep):
+    @dataclass(frozen=True)
+    class CommandConfig:
+        runs: int
+        commands: list[Command]
+        tag: str
+
+    @dataclass(frozen=True)
+    class Context:
+        runs: int
+        commands: list[Command]
+        tag: str
+        log_providers: list[LogProvider]
+        measurements: list[Measurement]
+
+    def __init__(self, host: SSHHost, command_config: CommandConfig, log_providers: list[LogProvider],
+                 measurements: list[Measurement]):
+        super().__init__("host command", host)
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._context = HostCommandStep.Context(runs=command_config.runs, commands=command_config.commands,
+                                                tag=command_config.tag,
+                                                log_providers=log_providers, measurements=measurements)
+        self._resources_path = None
+        self._environment = None
+        self._executor = None
+
+    def prepare(self, environment: ExperimentEnvironment, resources: ExperimentResources):
+        super().prepare(environment, resources)
+        self._resources_path = resources.resources_path()
+        self._environment = environment
+
+    def start(self, executor: Executor) -> None:
+        super().start(executor)
+        self._executor = executor
+
+    def _execute_commands(self, connection: Connection):
+        resources_path = self._resources_path / self._host.host_name / self._context.tag
+        resources_path.mkdir(parents=True, exist_ok=True)
+
+        self._logger.info("On host: %s execute %d command(s)", self._host.host, len(self._context.commands))
+
+        with ExitStack() as step_stack:
+            for measurement in self._context.measurements:
+                step_stack.enter_context(measure(measurement, self._environment, self._executor))
+            for run in range(self._context.runs):
+                self._execute_run(run, resources_path, connection)
+
+        self._logger.info("All commands executed")
+
+    def _execute_run(self, run: int, resources_path: Path, connection: Connection):
+        self._logger.info("Start run %d/%d", run + 1, self._context.runs)
+        run_resources_path = resources_path / ("run_%03d" % (run + 1))
+        run_resources_path.mkdir(parents=True, exist_ok=True)
+
+        with ExitStack() as run_stack:
+            for log_provider in self._context.log_providers:
+                run_stack.enter_context(log_provider.start_log(run_resources_path))
+
+            timings_resources_path = run_resources_path / "timings.csv"
+            for command in self._context.commands:
+                command.execute(connection, timings_resources_path)
