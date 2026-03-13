bd2240d2e881da936abb816c8b2a13a67548fb86
Ralf Sieger <sieger@fzi.de>
2026-03-13 08:51:59 +0100

    simplified the code

--- experiment/app/experiment/ssh_manager.py
+++ experiment/app/experiment/ssh_manager.py
@@ -21,23 +21,18 @@ class ConnectionEntry:
 
 
 class SSHManager(ExitStack):
-
     def __init__(self):
         super().__init__()
         self._logger = logging.getLogger(self.__class__.__name__)
-        self._connections: dict[str, ConnectionEntry] = {}
-
-    def _build_entry_name(self, host_info: HostInfo) -> str:
-        return f"{host_info.user}@{host_info.host_name}"
+        self._connections: dict[HostInfo, ConnectionEntry] = {}
 
     def register_ssh_connection(self, user: str, host: str) -> None:
         host_info = HostInfo(user, host)
-        entry_name = self._build_entry_name(host_info)
-        if entry_name in self._connections:
+        if host_info in self._connections:
             return
         password = getpass(f'SSH password for {user}@{host}: ')
         entry = ConnectionEntry(host_info=host_info, password=password, connection=None)
-        self._connections[entry_name] = entry
+        self._connections[host_info] = entry
 
     def _create_connection(self, entry: ConnectionEntry) -> Connection:
         connect_kwargs = {
@@ -47,8 +42,7 @@ class SSHManager(ExitStack):
 
     def get_ssh_connection(self, user: str, host: str) -> Connection:
         host_info = HostInfo(user, host)
-        entry_name = self._build_entry_name(host_info)
-        entry = self._connections[entry_name]
+        entry = self._connections[host_info]
         if not entry.connection:
             entry.connection = self._create_connection(entry)
             self.enter_context(entry.connection)
