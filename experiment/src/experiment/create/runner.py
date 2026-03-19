dcb19d9ddf1b9e70713ef6168331abf7c5a5beff
Ralf Sieger <sieger@fzi.de>
2026-03-19 12:28:56 +0100

    added logging of experiment framework version

--- experiment/src/experiment/create/runner.py
+++ experiment/src/experiment/create/runner.py
@@ -4,6 +4,7 @@ from contextlib import contextmanager
 import shutil
 
 from experiment.ssh import ConnectionFactory
+from experiment._version import version, commit_id
 from .experiment_loader import ExperimentLoader
 from .user_connection_factory import PasswordConnectionFactory, PrivateKeyConnectionFactory
 
@@ -26,6 +27,7 @@ class Runner:
         self._logger.debug("copy experiment module to resource folder")
         shutil.copy(experiment_module.resolve(), resources / experiment_module.name)
         with self._add_logfile(resources / "experiment.log"):
+            self._log_version_infos()
             self._logger.info("Start experiment: %s", experiment_module.stem)
             try:
                 metrics_server_address = (args.host, args.port)
@@ -51,3 +53,6 @@ class Runner:
         yield
         for logger in file_logger:
             logging.getLogger(logger).removeHandler(handler)
+
+    def _log_version_infos(self):
+        self._logger.debug(f"Using power-measurement-experiment v{version} ({commit_id})")
