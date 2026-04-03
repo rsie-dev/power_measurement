87f9a01427b57cdd786265470a7bb1be06512212
Ralf Sieger <sieger@fzi.de>
2026-04-03 13:23:23 +0200

    introduced BaseConnectionFactory

--- experiment/src/experiment/create/user_connection_factory.py
+++ experiment/src/experiment/create/user_connection_factory.py
@@ -3,6 +3,7 @@ from getpass import getpass
 from pathlib import Path
 import socket
 from typing import Optional
+from abc import abstractmethod
 
 from fabric import Connection
 import paramiko
@@ -10,7 +11,16 @@ import paramiko
 from experiment.ssh import HostInfo, ConnectionFactory
 
 
-class PasswordConnectionFactory(ConnectionFactory):
+class BaseConnectionFactory(ConnectionFactory):
+    def create_connection(self, host_info: HostInfo) -> Connection:
+        return self._connection(host_info)
+
+    @abstractmethod
+    def _connection(self, host_info: HostInfo) -> Connection:
+        pass
+
+
+class PasswordConnectionFactory(BaseConnectionFactory):
     def __init__(self):
         super().__init__()
         self._cache: dict[HostInfo, str] = {}
@@ -21,14 +31,14 @@ class PasswordConnectionFactory(ConnectionFactory):
         password = getpass(f'SSH password for {host_info.user}@{host_info.host_name}: ')
         self._cache[host_info] = password
 
-    def create_connection(self, host_info: HostInfo) -> Connection:
+    def _connection(self, host_info: HostInfo) -> Connection:
         connect_kwargs = {
             "password": self._cache[host_info],
         }
         return Connection(host_info.host_name, user=host_info.user, connect_kwargs=connect_kwargs)
 
 
-class PrivateKeyConnectionFactory(ConnectionFactory):
+class PrivateKeyConnectionFactory(BaseConnectionFactory):
     def __init__(self, ssh_key: Path):
         self._logger = logging.getLogger(self.__class__.__name__)
         self._ssh_key = ssh_key
@@ -70,7 +80,7 @@ class PrivateKeyConnectionFactory(ConnectionFactory):
                 except paramiko.ssh_exception.AuthenticationException:
                     return False
 
-    def create_connection(self, host_info: HostInfo) -> Connection:
+    def _connection(self, host_info: HostInfo) -> Connection:
         connect_kwargs = {
             "key_filename": str(self._ssh_key)
         }
