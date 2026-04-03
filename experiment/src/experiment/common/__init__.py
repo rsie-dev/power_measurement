91098641e58180357b8649908c60a46ef12e8553
Ralf Sieger <sieger@fzi.de>
2026-04-03 10:54:13 +0200

    removed now superfluous SignalHandler

--- experiment/src/experiment/common/__init__.py
+++ experiment/src/experiment/common/__init__.py
@@ -1,8 +1,7 @@
-from .signal_handler import SignalHandler
 from .shutdown_handler import ShutdownHandler
 from .host import Host, SSHHost
 
 __all__ = [
-    "SignalHandler", "ShutdownHandler",
+    "ShutdownHandler",
     "Host", "SSHHost",
 ]
