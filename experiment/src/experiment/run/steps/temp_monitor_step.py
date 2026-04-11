438607fa6c180eca14137c8d429ecea73739ffa1
Ralf Sieger <sieger@fzi.de>
2026-04-11 11:26:08 +0200

    added temperature threshold evaluation

--- experiment/src/experiment/run/steps/temp_monitor_step.py
+++ experiment/src/experiment/run/steps/temp_monitor_step.py
@@ -1,6 +1,7 @@
 import logging
 from concurrent.futures import Executor
 from typing import List
+import datetime
 
 from usb_multimeter import ElectricalMeasurement
 
@@ -14,14 +15,21 @@ from .measurement_step import MeasurementAbort
 
 
 class TempMonitorStep(Step, Logger, MeasurementAbort):
-    def __init__(self, log_dispatcher: LogDispatcher[ElectricalMeasurement], max_temp_delta: float):
+    def __init__(self, log_dispatcher: LogDispatcher[ElectricalMeasurement], max_temp_delta: float,
+                 min_duration: datetime.timedelta | None = None):
         super().__init__("temperature monitor")
         self._logger = logging.getLogger(self.__class__.__name__)
         self._log_dispatcher = log_dispatcher
         self._max_temp_delta = max_temp_delta
+        if min_duration is None:
+            min_duration = datetime.timedelta(seconds=2)
+        self._min_duration = min_duration
+        self._threshold = None
+        self._start_time = None
+        self._abort_flag = False
 
     def abort_measurement(self) -> bool:
-        return False
+        return self._abort_flag
 
     def execute(self, runtime: ExperimentRuntime) -> None:
         pass
@@ -30,15 +38,33 @@ class TempMonitorStep(Step, Logger, MeasurementAbort):
         pass
 
     def start(self, executor: Executor) -> None:
-        self._logger.warning("temp monitor start")
+        self._logger.debug("temperature monitor start")
         self._log_dispatcher.register_logger(self)
 
     def stop(self, runtime: ExperimentRuntime) -> None:
         self._log_dispatcher.unregister_logger(self)
-        self._logger.warning("temp monitor stop")
+        self._logger.debug("temperature monitor stop")
 
     def _log_measurement(self, data: ElectricalMeasurement) -> None:
-        self._logger.warning("Temp: %0.3f°C", data.temperature)
+        if self._threshold is None:
+            initial_temperature = data.temperature
+            self._threshold = initial_temperature + self._max_temp_delta
+            self._logger.info("initial temp: %0.2f°C -> threshold: %0.2f°C", initial_temperature, self._threshold)
+            return
+
+        now = datetime.datetime.now()
+
+        if data.temperature > self._threshold:
+            if self._start_time is None:
+                self._logger.warning("temp is above threshold (%0.2f°C): %0.3f",
+                                     self._threshold, data.temperature)
+                self._start_time = now
+            elif now - self._start_time > self._min_duration:
+                self._logger.fatal("temp is above threshold %0.2f°C for more than %s s",
+                                   self._threshold, self._min_duration)
+                self._abort_flag = True
+        else:
+            self._start_time = None
 
     def log(self, data: List[ElectricalMeasurement]) -> None:
         self._log_measurement(data[-1])
