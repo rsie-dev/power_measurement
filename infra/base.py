c970762ab4ac20fcdf70b7a51f537a03910c976e
Ralf Sieger <sieger@fzi.de>
2026-05-29 09:50:57 +0200

    added tree installation

--- infra/base.py
+++ infra/base.py
@@ -32,7 +32,7 @@ def base():
 
     apt.packages(
         name="Install base packages",
-        packages=["fish", "vim", "less", "tmux", "wget", "lm-sensors", "duf", "bat", "time", "ncdu"],
+        packages=["fish", "vim", "less", "tmux", "wget", "lm-sensors", "duf", "bat", "time", "ncdu", "tree"],
         no_recommends=True,
         _sudo=True,
     )
