15d3a89fc8080de9067a73a533d3aab852186f4b
Ralf Sieger <sieger@fzi.de>
2026-09-08 10:55:12 +0200

    fixed import order

--- experiment/src/experiment/run/steps/measurement_step.py
+++ experiment/src/experiment/run/steps/measurement_step.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 from experiment.api import Command
 from experiment.common import SSHHost
+from experiment.common import MeasurementAbort
 from experiment.run.base import ExperimentEnvironment
 from experiment.run.base import ExperimentResources
 from experiment.run.base import ExperimentRuntime
@@ -17,7 +18,6 @@ from experiment.run.steps.measurement import measure, Measurement
 from experiment.run.log import LogProvider
 from .host_command_step import BaseHostCommandStep
 from .log_redirect import logging_redirect_tqdm
-from experiment.common import MeasurementAbort
 
 
 class TotalTimeTqdm(tqdm):
