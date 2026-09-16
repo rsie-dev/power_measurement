36635d486c5ee956a6dad831546ab7c4533b1328
Ralf Sieger <sieger@fzi.de>
2026-09-16 09:15:03 +0200

    made the log dispatcher thread safe

--- experiment/src/experiment/run/log/log_dispatcher.py
+++ experiment/src/experiment/run/log/log_dispatcher.py
@@ -1,4 +1,5 @@
 from typing import TypeVar
+from threading import RLock
 
 from .logger import Logger
 
@@ -8,13 +9,19 @@ T = TypeVar('T')
 class LogDispatcher(Logger[T]):
     def __init__(self):
         self._loggers: list[Logger[T]] = []
+        self._lock = RLock()
 
     def register_logger(self, logger: Logger[T]):
-        self._loggers.append(logger)
+        with self._lock:
+            self._loggers.append(logger)
 
     def unregister_logger(self, logger: Logger[T]):
-        self._loggers.remove(logger)
+        with self._lock:
+            self._loggers.remove(logger)
 
     def log(self, data: T | list[T]) -> None:
-        for logger in self._loggers:
+        with self._lock:
+            loggers = self._loggers[:]
+
+        for logger in loggers:
             logger.log(data)
