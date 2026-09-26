a782b71dba7f8b6b0475a9ea292ca102acff3343
Ralf Sieger <sieger@fzi.de>
2026-09-26 09:45:17 +0200

    enhanced logging

--- experiment/src/experiment/run/steps/time_delta_step.py
+++ experiment/src/experiment/run/steps/time_delta_step.py
@@ -1,5 +1,5 @@
 import logging
-import datetime
+import datetime as dt
 
 import ntplib
 import humanize
@@ -23,25 +23,33 @@ class TimeDeltaStep(Step):
         self._time_delta_path = resources.resources_path() / "dut_time_delta.txt"
 
     def execute(self, runtime: ExperimentRuntime):
-        offset_delta, remote_time = self._get_ntp_delta()
+        offset_delta, destination, local, remote = self._get_ntp_delta()
 
-        remote_local = remote_time.astimezone()
-        remote_time_str = remote_local.isoformat(sep=" ", timespec="milliseconds")
-        self._logger.info("Time on %s: %s", self._host.host, remote_time_str )
+        precision = "microseconds"
+        local_time = local.astimezone()
+        local_time_str = local_time.isoformat(timespec=precision)
 
-        microseconds = offset_delta / datetime.timedelta(microseconds=1)
+        remote_time = remote.astimezone()
+        remote_time_str = remote_time.isoformat(timespec=precision)
+
+        self._logger.info("Time local:  %s", local_time_str)
+        self._logger.info("Time device: %s", remote_time_str)
+
+        microseconds = offset_delta / dt.timedelta(microseconds=1)
         status = "behind" if microseconds < 0 else "ahead"
-        delta_str = humanize.precisedelta(offset_delta, minimum_unit="microseconds")
+        delta_str = humanize.precisedelta(offset_delta, minimum_unit=precision)
         self._logger.info("%s is %s %s", self._host.host_name, status, delta_str)
 
         with self._time_delta_path.open(mode="w", encoding="utf-8") as f:
             iso_str = isodate.duration_isoformat(offset_delta)
             f.write(f'delta = "{iso_str}"')
 
-    def _get_ntp_delta(self) -> tuple[datetime.timedelta, datetime.datetime]:
+    def _get_ntp_delta(self) -> tuple[dt.timedelta, dt.datetime, dt.datetime, dt.datetime]:
         ntp_client = ntplib.NTPClient()
         self._logger.debug("get time diff to: %s", self._host.host)
         response = ntp_client.request(self._host.host, version=4)  # v4 is common
-        offset_delta = datetime.timedelta(seconds=response.offset)
-        destination = datetime.datetime.fromtimestamp(response.tx_time, tz=datetime.timezone.utc)
-        return offset_delta, destination
+        offset_delta = dt.timedelta(seconds=response.offset)
+        destination = dt.datetime.fromtimestamp(response.tx_time, tz=dt.timezone.utc)
+        local = dt.datetime.fromtimestamp(response.dest_time, tz=dt.timezone.utc)
+        remote = dt.datetime.fromtimestamp(response.dest_time + response.offset, tz=dt.timezone.utc)
+        return offset_delta, destination, local, remote
